481d7080df661217332358ea651c2d288e9fe1fe
Gluccky <andreyzaycev2875@gmail.com>
2025-05-18 20:25:22 +0500

    kek

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-"some code"
+import flet as ft
+ 
+ 
+def main(page: ft.Page):
+ 
+    page.title = "Flet Trello clone"
+    page.padding = 0
+    page.bgcolor = ft.Colors.BLUE_GREY_200
+    app = TrelloApp(page)
+    page.add(app)
+    page.update()
+ 
+class TrelloApp:
+    def __init__(self, page: ft.Page):
+        self.page = page 
+        self.appbar_items = [
+            ft.PopupMenuItem(text="Login"),
+            ft.PopupMenuItem(),
+            ft.PopupMenuItem(text="Settings")
+        ]
+
+
+            
+ft.app(main)
