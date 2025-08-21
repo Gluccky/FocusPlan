544134d68286eddbc20add69767b437900be0fec
Gluccky <andreyzaycev2875@gmail.com>
2025-08-21 08:11:59 +0500

    21.08

--- components/notes.py
+++ components/notes.py
@@ -1,9 +1,51 @@
 import flet as ft
 
 
-notespage = ft.Container(
-    bgcolor=ft.Colors.BLUE_400,
-    expand=True,
+def notes_switch(name: str):
+        if name == "notesroutine":
+            notespage.controls.append(notesroutine)
+            notespage.update()
+        elif name == "notesimportant":
+            notespage.controls.append(notesimportant)
+            notespage.update()
+
+menubar = ft.Row(
+    alignment=ft.MainAxisAlignment.CENTER,
+    controls=[
+        ft.ElevatedButton(
+            text="Рутинные",
+        bgcolor=ft.Colors.WHITE,
+        on_click=lambda e: notes_switch("notesroutine")
+        ),
+        ft.ElevatedButton(
+            text="Важные",
+        bgcolor=ft.Colors.WHITE,
+        on_click=lambda e: notes_switch("notesimportant")
+        )
+    ]
+)
+
+notesimportant = ft.Container(
+    content=ft.ElevatedButton(
+            text="addimportant",
+            bgcolor=ft.Colors.BLUE,
+            color=ft.Colors.WHITE,
+    )
+)
+
+notesroutine = ft.Container(
+    content=ft.ElevatedButton(
+            text="addroutine",
+            bgcolor=ft.Colors.BLUE,
+            color=ft.Colors.WHITE,
+    )
+)
+
+notespage = ft.Column(
+    controls=[
+        menubar,
+        notesroutine
+    ]
 )
 
 def notes_add():
