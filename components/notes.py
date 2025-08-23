d242aa83bbb499e64734663bfb630397fe72ba30
Gluccky <andreyzaycev2875@gmail.com>
2025-08-23 08:13:27 +0500

    2308

--- components/notes.py
+++ components/notes.py
@@ -1,12 +1,27 @@
 import flet as ft
 
 
+def notes_add(e: ft.ControlEvent):
+        text = e.control.value.strip()
+        if text:
+            notespagenotes.controls.append(
+                ft.Container(
+                    content=ft.Text(text),
+                    bgcolor=ft.Colors.AMBER_100,
+                    padding=10,
+                    margin=5,
+                    border_radius=5
+                )
+            )
+            e.control.value = ""
+            notespagenotes.update()
+
 def notes_switch(name: str):
         if name == "notesroutine":
-            notespage.controls.append(notesroutine)
+            notesbuttons.content = notesroutine
             notespage.update()
         elif name == "notesimportant":
-            notespage.controls.append(notesimportant)
+            notesbuttons.content = notesimportant
             notespage.update()
 
 menubar = ft.Row(
@@ -34,22 +49,31 @@ notesimportant = ft.Container(
 )
 
 notesroutine = ft.Container(
-    content=ft.ElevatedButton(
-            text="addroutine",
-            bgcolor=ft.Colors.BLUE,
-            color=ft.Colors.WHITE,
+        content=ft.ElevatedButton(
+        on_click=lambda e: (notespagenotes.controls.append(ft.TextField(on_submit=notes_add())), notespage.update()),
+        text="addroutine",
+        bgcolor=ft.Colors.BLUE,
+        color=ft.Colors.WHITE,
     )
+)
+
+notesbuttons = ft.Container(
+     
+)
+
+notespagenotes = ft.Column(
+     expand=True
+     
 )
 
 notespage = ft.Column(
     controls=[
         menubar,
-        notesroutine
+        notesbuttons,
+        notespagenotes
     ]
 )
 
-def notes_add():
-    pass
 
 def notes_delete():
     pass
